e1f873dc4114b26af5dd06f63ddf3dadb170c095
sebastien <heligon.seb@gmail.com>
2022-08-04 18:09:04 +0200

    text const in class with sql for symphony insight

--- src/models/repositories/PostRepository.php
+++ src/models/repositories/PostRepository.php
@@ -9,7 +9,7 @@ use App\models\entities\Post;
 class PostRepository 
 {
     private $db;
-    private $table="post";
+    const table="post";
 
     public function __construct()
     {
@@ -26,10 +26,10 @@ class PostRepository
     public function add($title, $intro, $content) {
         $id = 2;
 
-        $query = 'INSERT INTO :table(title,intro,content,creat_date,edit_date,user_id) VALUES (:title, :intro, :content, NOW(), null, :id)';
+        $query = 'INSERT INTO ' . self::table . '(title,intro,content,creat_date,edit_date,user_id) VALUES (:title, :intro, :content, NOW(), null, :id)';
 
         $q = $this->db->prepare($query);
-        $q->execute(['table' => $this->table, 'title'=>$title, 'intro'=>$intro, 'content'=>$content, 'id'=> $id]);
+        $q->execute(['title'=>$title, 'intro'=>$intro, 'content'=>$content, 'id'=> $id]);
     }
 
     /**
@@ -53,9 +53,11 @@ class PostRepository
      */
     public function getPostList() {
         $query = 'SELECT * FROM :table';
+        $query = 'SELECT * FROM ' . $this->table;
         
         $q = $this->db->prepare($query);
-        $q->execute(['table' => $this->table]);
+        //$q->execute(['table' => $this->table]);
+        $q->execute();
         $result = $q->fetchAll(\PDO::FETCH_CLASS, Post::class);
 
         return $result;
--- src/views/error/unknown.php
+++ src/views/error/unknown.php
@@ -3,7 +3,7 @@
 <div class="error errorUnknown">
     <a href="/public" class="btn btn-outline-secondary mt-4 ms-4">
         <i class="bi bi-arrow-left"></i>
-        Acceuil
+        Accueil
     </a>
 </div>
 
