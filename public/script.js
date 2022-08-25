0f89577ecae80143f68b20315220a6e37cddf4cf
sebastien <heligon.seb@gmail.com>
2022-08-25 10:15:37 +0200

    delete modal and use full page / finish css mobile

--- public/script.js
+++ public/script.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // burger menu
     let btnBurger = document.querySelector('.ico-btn');
 
     btnBurger.addEventListener('click', () => {
         btnBurger.classList.toggle('is-active');
+    });
+
+    // ajout de commentaire pour un post
+    let addCommentButton = document.querySelector('#addCommentButton');
+    let closeCommentButton = document.querySelector('#closeCommentButton');
+    let cardAddComment = document.querySelector('#addComment');
+
+    addCommentButton.addEventListener('click', () => {
+        cardAddComment.classList.remove('d-none');
+    });
+
+    closeCommentButton.addEventListener('click', () => {
+        cardAddComment.classList.add('d-none');
     });
 });
 
